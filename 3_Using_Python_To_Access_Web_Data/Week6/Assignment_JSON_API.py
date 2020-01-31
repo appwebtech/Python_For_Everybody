7d99cddbd6715493e48b385952ff014b8ce93817
Joseph M Mwania <josembi@gmail.com>
2020-01-31 21:37:27 +0000

    Assignment JSON & API's

---
+++ 3_Using_Python_To_Access_Web_Data/Week6/Assignment_JSON_API.py
@@ -0,0 +1,55 @@
+# Dr Chuck had supplied an old API end point which inadvertently threw errors from
+# the grading system because the placement_id was an old one. So I re-did the code
+# again and had to import JSON on it's own because apparently in Python 2 there was
+# no point to have it imported and in Python 3 urllib needed to get .request appended to it.
+
+import ssl
+import json
+import urllib.request, urllib.parse, urllib.error
+
+
+
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+joe = ssl.create_default_context()
+joe.check_hostname = False
+joe.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=joe)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
